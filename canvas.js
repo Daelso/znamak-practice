3ad87c2833cc3dd38cb0d940774682bbdea030b5
Chase Murtaugh <chasemurtaugh@gmail.com>
2024-04-15 09:40:00 -0400

    fat sprite

--- canvas.js
+++ canvas.js
@@ -9,8 +9,8 @@ const colorCheck = (gridItem) => {
 const imageLinks = {
   "000": "https://i.imgur.com/U7mevkM.png", //not fat male front
   "001": "https://i.imgur.com/w8rdJci.png", // not fat male back
-  "010": "image2.png", // fat male front
-  "011": "image3.png", //fat male back
+  "010": "https://i.imgur.com/Ab1T1KA.png", // fat male front
+  "011": "https://i.imgur.com/7waSJ7M.png", //fat male back
   100: "image4.png", //not fat female front
   101: "image5.png", //not fat female back
   110: "image6.png", //fat female front
@@ -20,8 +20,8 @@ const imageLinks = {
 const startingPoints = {
   "000": { tatStartX: 40, tatEndX: 44, tatStartY: 44, tatEndY: 48 }, //not fat male front
   "001": { tatStartX: 40, tatEndX: 44, tatStartY: 44, tatEndY: 48 }, // not fat male back
-  "010": { startX: 40, endX: 44, startY: 44, endY: 48 }, // fat male front
-  "011": { startX: 40, endX: 44, startY: 44, endY: 48 }, //fat male back
+  "010": { tatStartX: 40, tatEndX: 44, tatStartY: 48, tatEndY: 52 }, // fat male front
+  "011": { tatStartX: 40, tatEndX: 44, tatStartY: 48, tatEndY: 52 }, //fat male back
   100: { startX: 40, endX: 44, startY: 44, endY: 48 }, //not fat female front
   101: { startX: 40, endX: 44, startY: 44, endY: 48 }, //not fat female back
   110: { startX: 40, endX: 44, startY: 44, endY: 48 }, //fat female front
